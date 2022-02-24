cfaba6c96ea1e96a4d4c22e28488b0d851a29acc
Joey Santoro <joseph.santoro@duke.edu>
2022-02-24 11:25:18 -0800

    fix import error

--- src/test/Integration.t.sol
+++ src/test/Integration.t.sol
@@ -6,7 +6,7 @@ import {MockERC20, ERC20} from "solmate/test/utils/mocks/MockERC20.sol";
 import {MockMarket} from "./mocks/MockMarket.sol";
 import {MockBooster} from "./mocks/MockBooster.sol";
 
-import "../FuseFlywheelCore.sol";
+import "../fuse-compatibility/FuseFlywheelCore.sol";
 import {FlywheelStaticRewards} from "../rewards/FlywheelStaticRewards.sol";
 
 interface Comptroller {
