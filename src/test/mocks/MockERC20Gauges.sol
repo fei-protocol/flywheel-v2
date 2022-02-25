f0fae6bbe642e96c40847fb5d98c71cc8b374150
Joey Santoro <joseph.santoro@duke.edu>
2022-02-25 10:12:47 -0800

    mock ERC20 Gauges

---
+++ src/test/mocks/MockERC20Gauges.sol
@@ -0,0 +1,19 @@
+
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.0;
+
+import {ERC20Gauges, ERC20, Auth, Authority} from "../../token/ERC20Gauges.sol";
+
+contract MockERC20Gauges is ERC20Gauges {
+    constructor(
+        address _owner
+    ) ERC20("Token", "TKN", 18) Auth(_owner, Authority(address(0))) {}
+
+    function mint(address to, uint256 value) public virtual {
+        _mint(to, value);
+    }
+
+    function burn(address from, uint256 value) public virtual {
+        _burn(from, value);
+    }
+}
